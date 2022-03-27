0888ffef48de3cd43d5ad312a8465a6e43def8bc
micaelamedina <micadm237@gmail.com>
2022-03-27 15:28:37 -0300

    Db Created

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID, //36 caracteres alfanuméricos aleatorios.
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.INTEGER
+    },
+    healthScore: {
+      type: DataTypes.INTEGER
+    },
+    steps: {
+      type: DataTypes.TEXT
+    },
+    createdByDB: { //Con esta propiedad validamos de donde mostrar la receta, si desde la DB o desde la API.
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
+
