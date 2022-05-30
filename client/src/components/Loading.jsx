96ee9cc83931851c2f85b8145e50eb518cf85023
micaelamedina <micadm237@gmail.com>
2022-05-30 20:14:11 -0300

    cambios en CSS

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,8 +3,8 @@ import s from "./styles/Loading.module.css"
 
 export default function Loading() {
     return(
-        <div className={s.textoLoading}>
-            <h1>Loading... </h1>
+        <div className={s.errorDetail}>
+            <h1 className={s.h3ErrDetail}>Loading... </h1>
         </div>
     )
 }
